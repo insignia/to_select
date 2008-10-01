05e4b6502e13134580e9a025df61b95302288a41
Carlos Kozuszko <carlos@insignia4u.com>
2008-10-01 15:56:20 -0300

    Adding specs

---
+++ spec/active_record_extensions_spec.rb
@@ -0,0 +1,40 @@
+module ActiveRecord
+  class Base
+  end
+end
+
+require File.dirname(__FILE__) + '/../lib/active_record_extensions.rb'
+
+describe ActiveRecord::Base do
+
+  before :each do
+    m1, m2 = mock('model1'), mock('model2')
+    m1.stub!(:id).and_return(1)
+    m1.stub!(:name).and_return('model1')
+    m1.stub!(:some_field).and_return('some_field1')
+    m2.stub!(:id).and_return(2)
+    m2.stub!(:name).and_return('model2')
+    m2.stub!(:some_field).and_return('some_field2')
+    @records = [m1, m2]
+  end
+
+  describe 'with no conditions' do
+
+    it "should return a new array with [name, id] touples" do
+      ActiveRecord::Base.should_receive(:find).with(:all, :conditions => nil).and_return(@records)
+      ActiveRecord::Base.to_select == [['model1', 1], ['model2', 2]]
+    end
+
+    it "should return a new array with [some_field, id] touples" do
+      ActiveRecord::Base.should_receive(:find).with(:all, :conditions => nil).and_return(@records)
+      ActiveRecord::Base.to_select(:some_field) == [['some_field1', 1], ['some_field2', 2]]
+    end
+
+    it "should return a new array with [some_field, id] touples with the records filtered by some_condition" do
+      ActiveRecord::Base.should_receive(:find).with(:all, :conditions => 'some_condition').and_return(@records)
+      ActiveRecord::Base.to_select(:some_field, 'some_condition') == [['some_field1', 1], ['some_field2', 2]]
+    end
+
+  end
+
+end
---
+++ spec/array_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/../lib/array.rb'
+
+describe Array do
+
+  before do
+    m1, m2 = mock('model1'), mock('model2')
+    m1.stub!(:id).and_return(1)
+    m1.stub!(:name).and_return('model1')
+    m1.stub!(:some_field).and_return('some_field1')
+    m2.stub!(:id).and_return(2)
+    m2.stub!(:name).and_return('model2')
+    m2.stub!(:some_field).and_return('some_field2')
+    @records = [m1, m2]
+  end
+
+  it "should return a new array with [name, id] touples" do
+    @records.to_select.should == [['model1', 1], ['model2', 2]]
+  end
+
+  it "should return a new array with [some_field, id] touples" do
+    @records.to_select(:some_field).should == [['some_field1', 1], ['some_field2', 2]]
+  end
+
+end
